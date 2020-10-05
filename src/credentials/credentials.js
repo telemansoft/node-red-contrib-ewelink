3f416d36748340120d6894451b0c3ace221ca399
telemansoft <38874720+telemansoft@users.noreply.github.com>
2020-10-05 13:39:07 +0800

    Update credentials.js
    修改Email属性为username属性，username属性为ewelink帐户注册的用户名，可以是电子邮件或电话号码（电话号码应该带区域号，例如‘+8612345678901’）。

--- src/credentials/credentials.js
+++ src/credentials/credentials.js
@@ -13,20 +13,32 @@ module.exports = function (RED) {
   function CredentialsNode(config) {
     // Create the node
     RED.nodes.createNode(this, config);
-    
+
+    var re = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
     // Unpack credentials
-    this.email = this.credentials.email;
+    if re.test(this.credentials.username) {
+      this.email = this.credentials.username;
+      this.phoneNumber = null;
+    } else {
+      this.phoneNumber = this.credentials.username;
+      this.email = null;
+    };    
     this.password = this.credentials.password;
     this.region = this.credentials.region;
     
     // Initialize eWeLink
-    this.connection = new ewelink(this.credentials);
+    this.connection = new ewelink({
+      email: this.email,
+      phoneNumber: this.phoneNumber,
+      password: this.password,
+      region: this.region,
+    });
   }
 
   // Register node
   RED.nodes.registerType('ewelink-credentials', CredentialsNode, {
     credentials: {
-      email: { type: 'email' },
+      username: { type: 'text' },
       password: { type: 'password' },
       region: { type: 'text' }
     }
